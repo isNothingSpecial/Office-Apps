1aba053c1f7be95c9ae0e52cbb3531e0c374d79e
Bagus Rahma AC <46398272+isNothingSpecial@users.noreply.github.com>
2025-07-24 12:19:19 +0700

    Update Launcher.py

--- Launcher.py
+++ Launcher.py
@@ -1,19 +1,19 @@
 import streamlit as st
 
-st.set_page_config(page_title="PDF Tools Dashboard", layout="wide")
+st.set_page_config(page_title="PDF Toolkit Apps", layout="wide")
 
-st.title("📚 PDF Tools Dashboard")
-st.markdown("Pilih salah satu fitur di bawah ini:")
+st.title("📚 Dashboard")
+st.markdown("Pilih salah satu fitur yang ingin digunakan :")
 
 # Grid layout
 cols = st.columns(2)
 
 with cols[0]:
-    st.page_link("Merge_PDF.py", label="📎 Merge PDF", icon="📎")
+    st.page_link("Merge.py", label="📎 Merge PDF", icon="📎")
 
-    st.page_link("Split_PDF.py", label="✂️ Split PDF", icon="✂️")
+    st.page_link("Split.py", label="✂️ Split PDF", icon="✂️")
 
 with cols[1]:
-    st.page_link("Delete_Page.py", label="🗑️ Delete Page", icon="🗑️")
+    st.page_link("Delete.py", label="🗑️ Delete Page", icon="🗑️")
 
-    st.page_link("PDF_to_Word.py", label="📄 PDF to Word", icon="📄")
+    st.page_link("Word.py", label="📄 PDF to Word", icon="📄")
